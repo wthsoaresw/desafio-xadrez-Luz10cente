ca92a6a5e29c54c2681d1cc038661b8958dcefda
wthsoaresw <thiagosoares72474877@gmail.com>
2025-03-05 23:56:05 +0000

    criei matriz

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
 int main(){
- printf("Desafio Xadrez!\n");
- printf("Novo commit\n");
- 
-}    
+    int matriz[9][9] = {
+        
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 3, 3, 3, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+    };
+    
+
+    printf("a parte do navio na posição [7][7] é %d \n", matriz[7][7]); 
+    printf("a parte do navio na posição [7][6] é %d \n", matriz[7][6]);
+    printf("a parte do navio na posição [7][5] é %d \n", matriz[7][5]);
+   
+   
+    
+    return (0);
+}
+    
