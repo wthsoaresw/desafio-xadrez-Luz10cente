5fa5cbdee91b41a752f2647d8219bf2bf1ba61ae
wthsoaresw <thiagosoares72474877@gmail.com>
2025-03-06 23:26:27 -0300

    Add files via upload

---
+++ desafio xadrez 2.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("dasafio xadrez\n");
+    printf("conmit\n");
+    printf("\n");
+
+    int i = 1;
+    
+    while (i <= 5){
+        if(i % 1 <= 0)
+        {
+          printf("torre moveu %d casas para frente \n",i);                  
+        }
+
+        i++;
+    }  
+    printf("\n");
+
+    int j = 1;
+ 
+    do {
+        printf("bispo moveu %d casa para frente direita \n", j);
+        j++;
+
+    } while (j <= 5);
+
+    printf("\n");
+
+    for (int k = 1; k <= 8; k++)
+    {
+      printf("rainha moveu %d casas para esquerda \n", k);
+    }
+
+    
+
+   
+   return 0; 
+}
